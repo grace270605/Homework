b194b2c71cb2bc27c543f308786b64a14a2d2339
MelindaRatuEda <melinda24@jiu.ac>
2026-03-29 16:19:24 +0700

    membuat kelas menu item berisi atribut name price dan stock dan juga kategori  yangh di ambil dari kelas Catergory dan juga constractornya

---
+++ MenuItem.java
@@ -0,0 +1,22 @@
+class MenuItem {
+
+    private String name;
+    private double price;
+    private int stock;
+    private Category category;
+
+
+
+    public MenuItem(String name, double price, int stock, Category category) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+
+    }
+
+    public MenuItem() {
+
+    }
+}
+    
